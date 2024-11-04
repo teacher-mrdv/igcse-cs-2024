626eeb3186aac528a6bb3fa919101025a311b1d2
Marcos Drien <teacher.mrdv@gmail.com>
2024-11-04 12:46:53 +0800

    Monday update

--- class_examples/bin2den.py
+++ class_examples/bin2den.py
@@ -5,13 +5,17 @@ while not valid_binary:
     valid_binary = True # let's assume it's OK
     # then we check if any bit is not 0 or 1
     for bit in binary:
-        if bit != '0' and bit != '1':
+        if bit != '0' and bit != '1' and bit != ' ':
             valid_binary = False
+            print('Error - input is not a binary number! Please input again.')
             break
             # no point checking anymore if one digit isn't binary
+        #end if
+#end while
 for bit in binary:
-    denary = denary + int(bit)
-    denary = 2 * denary
-denary = denary / 2 # because we multiply by 2 one time too many
+    if bit != ' ':
+        denary = denary + int(bit)
+        denary = 2 * denary
+    #end if
+denary = int(denary / 2) # because we multiply by 2 one time too many
 print(f'Binary {binary} = Denary {denary}')
-    
---
+++ class_examples/primefactors.py
@@ -0,0 +1,10 @@
+a = int(input())
+for i in range(2, a+1):
+    if a % i == 0:
+        prime = True
+        for j in range(2, i):
+            if i % j == 0:
+                prime = False
+                break;
+        if prime == True:
+            print(i)
