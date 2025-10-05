aacec5fffe090dcb76278ee8f75096a492da70f0
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2025-10-05 19:10:39 +0800

    Revert "Update 2d_array_basics.py"
    This reverts commit 9a5148c92446e4df86b23c21f875b4478205fecd.

--- arrays/2d_array_basics.py
+++ arrays/2d_array_basics.py
@@ -1,5 +1,6 @@
 # 2D Arrays basics
 # Here we use lists to simulate arrays, again.
+# MANY THANKS to my MYP Computer Science G2-02 class (2025) for asking awesome questions!!!
 
 # this method creates a two dimensional array of the specified dimensions
 # (rows and columns), and initialises the array with the given initialValue
@@ -35,20 +36,51 @@ def printRow(array: list, rowIndex:int):
     if rowIndex >= rowLength:
         return None
     columnLength:int = len(array[rowIndex])
-    #for colIndex in range(columnLength):
-        #print(array[rowIndex][colIndex], end=' ')
-    print(array[rowIndex])
+    for colIndex in range(columnLength):
+        print(array[rowIndex][colIndex], end=' ')
+    #print(array[rowIndex])
     print()
+    
+# Task 1: complete this method so that it prints the left-to-right diagonal of an array
+# make sure to validate that the array is a square (number of rows=number of columns)
+def printDiagonal(array: list):
+    rowLength:int = len(array)
+    columnLength:int = len(array[rowLength-1]) # length of the last row also works
+    if rowLength != columnLength:
+        print('The array input is not a square, cannot extract a diagonal.')
+        return
+    for index in range(rowLength):
+        print(array[index][index], end=' ')
+    print()
+    return 
 
+# Task 2: complete this method so that it prints a specific COLUMN from an array
+# make sure to validate the given column as it has to be < the number of column
+def printColumn(array: list, columnIndex:int):
+    rowLength:int = len(array)
+    columnLength:int = len(array[0])
+    if columnIndex >= columnLength:
+        return
+    for rowIndex in range(rowLength):
+        print(array[rowIndex][columnIndex], end=' ')
+    print()
+    
+#################################################################################
+# Main Method ###################################################################
+#################################################################################
 
-# DECLARE arr2d : ARRAY[0:1, 0:3] OF INTEGER
-
+# PSEUDOCODE
+# DECLARE Arr2d : ARRAY[0:1, 0:3] OF INTEGER
 arr2d = create2DArray(2, 4, 0)
 print(arr2d)
 print()
 prettyPrintArray(arr2d)
 print()
-another2dArray = create2DArray(5, 4, None)
+# PSEUDOCODE and remarks:
+# DECLARE Another2dArray : ARRAY[0:4, 0:3] OF CHAR
+# Below: '' or None to initialise the array with empty strings or null/nothing;
+# Python doesn't have a char/character data type, only string
+another2dArray = create2DArray(5, 4, '')
 prettyPrintArray(another2dArray)
 print()
 another2dArray = [ ['0','1','2','3'], ['4','5','6','7'], ['8','9','A','B'], ['C','D','E','F'], ['G','H','I','J'] ]
@@ -57,12 +89,57 @@ columnLen = len(another2dArray[0])
 rowLen    = len(another2dArray)
 print(f'\nlength of rows = {rowLen}')
 print(f'length of each column = {columnLen}\n')
+printDiagonal(another2dArray)
 # Use the printRow function to print column #2 or another2dArray
+print('3rd row (index 2) of the array above: ', end='')
 printRow(another2dArray, 2)
+print('Last column (index columnLength-1) of the array above: ', end='')
+printColumn(another2dArray, columnLen-1)
+print()
+
 # diagonal if array is square
-if columnLen == rowLen:
-    
+squareArray = create2DArray(3, 3, 0)
+squareArray = [ [9,8,7], [6,5,4], [3,2,1] ]
+prettyPrintArray(squareArray)
+columnLen = len(squareArray[0])
+rowLen    = len(squareArray)
+print(f'\nlength of rows = {rowLen}')
+print(f'length of each column = {columnLen}\n')
+print('First column (index 0) of the array above: ', end='')
+printColumn(squareArray, 0)
+print('Last row (len(array)-1) of the array above: ', end='')
+printRow(squareArray, len(squareArray)-1)
+print('Diagonal of the array above: ', end='')
+printDiagonal(squareArray)
+print()
+
 # mirror another2dArray
 
 # transpose another2dArray
+# Work in progress--- convert to function!
+for c in range(len(another2dArray[0])):
+    for r in range(len(another2dArray)):
+        print(another2dArray[r][c], end=' ')
+    print()
 
+print()
+
+# Davi's question - processing a 2D array by column (and by rows first):
+prettyPrintArray(squareArray)
+print('\nProcessing rows:')
+for rowIndex in range(len(squareArray)):
+    suma: int = 0
+    for columnIndex in range(len(squareArray[0])):
+        x: int = squareArray[rowIndex][columnIndex]
+        suma += x
+        print(x, end=' ')
+    print(f' = {suma}')
+print('\nProcessing columns:')
+for columnIndex in range(len(squareArray[0])):
+    suma: int = 0
+    for rowIndex in range(len(squareArray)):
+        x: int = squareArray[rowIndex][columnIndex]
+        suma += x
+        print(x, end=' ')
+    print(f' = {suma}')
+print()
