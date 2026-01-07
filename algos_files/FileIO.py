5ab4cb871a0445eaea839af3c5c563faf2d58120
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2026-01-07 16:10:17 +0800

    Wednesday update

--- algos_files/FileIO.py
+++ algos_files/FileIO.py
@@ -25,6 +25,8 @@ def print_data(array: list) -> None:
         print()
     print()
 
+# sorts a 2D array by a chosen column. Look at the constants above.
+# note that the algorithm is the same, but we move entire rows
 def bubble_sort2D(array_data: list, column: int) -> list:
     array: list = copy.deepcopy(array_data)		# we have to do this in Python to leave the original array unmodified
     swapped: bool = True
@@ -34,32 +36,34 @@ def bubble_sort2D(array_data: list, column: int) -> list:
         index: int = 0
         for index in range(len(array) - 1 - passes):
             if array[index][column] > array[index + 1][column]:
-                temp = array[index]
+                temp: list = array[index]
                 array[index] = array[index + 1]
                 array[index + 1] = temp
                 swapped = True
     return array
 
+# reads a file (csv) and returns a 2D array/list with the contents of the CSV file
 def read_data(filename: str) -> list:			# read the contents of filename and store them in an array [list]
     data: list = []
     with open(filename, 'r') as dataFile:		# open the file for reading operations
         for line in dataFile:
             line = line.rstrip()                # remove trailing whitespace[s] from the end of a string
-            parsed_data = line.split(',')       # splits each CSV line into a 2D array/list
+            parsed_data: list = line.split(',')       # splits each CSV line into a 2D array/list
             data.append(parsed_data)
         data.pop(0)                             # remove the header of the CSV file
-    print_data(data)                            # optional, can be removed/commented out
+    #print_data(data)                           # optional, can be removed/commented out
     return data
 
+# writes a CSV file with the contents of a 2D array/list
 def write_data(filename: str, array: list) -> None:
     with open(filename, 'w') as dataFile:
         for line in array:
             line_to_write: str = ''
             for item in line:
                 line_to_write += item + ','
-            line_to_write = line_to_write[:len(line_to_write)-1]
-            print(line_to_write)
-            dataFile.write(line_to_write + '\n')
+            line_to_write = line_to_write[:len(line_to_write)-1] # remove the last comma
+            dataFile.write(line_to_write + '\n')# add a newline '\n' at the end of each line 
+            #print(line_to_write)               # optional, can be removed/commented out
     print(f'\n>>>{filename} saved successfully.')
 
 # main
